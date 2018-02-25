e955be89acc5f35cf5e58d30365f6271859d56fd
creaticoder <creaticoding@gmail.com>
2018-02-25 19:30:55 +0900

    fix(01_HelloWorld): debuging about frame exec

--- 01_helloWorld/src/main/java/com/creaticoding/springswing/tutorial/Application.java
+++ 01_helloWorld/src/main/java/com/creaticoding/springswing/tutorial/Application.java
@@ -1,74 +1,32 @@
 package com.creaticoding.springswing.tutorial;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.builder.*;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-public class Application extends JFrame {
+public class Application {
 
-    public Application() {
+	public static void main(String[] args) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.headless(false).web(WebApplicationType.NONE).run(args);
+	}
+}
 
-        initUI();
-    }
-	 private void initUI() {
+@Component
+class HelloWorld extends JFrame {
+	private static final long serialVersionUID = -651405855813691971L;
 
-	        JButton quitButton = new JButton("Quit");
-
-	        quitButton.addActionListener(new ActionListener()
-	        {
-	        	  public void actionPerformed(ActionEvent e)
-	        	  {
-	  	            System.exit(0);
-	        	  }
-	        });
-
-	        createLayout(quitButton);
-
-	        setTitle("Quit button");
-	        setSize(300, 200);
-	        setLocationRelativeTo(null);
-	        setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    }
-
-	    private void createLayout(JComponent... arg) {
-
-	        Container pane = getContentPane();
-	        GroupLayout gl = new GroupLayout(pane);
-	        pane.setLayout(gl);
-
-	        gl.setAutoCreateContainerGaps(true);
-
-	        gl.setHorizontalGroup(gl.createSequentialGroup()
-	                .addComponent(arg[0])
-	        );
-
-	        gl.setVerticalGroup(gl.createSequentialGroup()
-	                .addComponent(arg[0])
-	        );
-	    }
-
-	    public static void main(String[] args) {
-
-	        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class)
-	                .headless(false).run(args);
-
-	        EventQueue.invokeLater(() -> {
-	            Application ex = ctx.getBean(Application.class);
-	            ex.setVisible(true);
-	        });
-	    }
+	HelloWorld() {
+		setTitle("Hello World");
+		Label label1 = new Label("Hello World", Label.CENTER);
+		label1.setFont(new Font("Consolas", Font.BOLD, 20));
+		this.setSize(500, 200);
+		this.add(label1);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
